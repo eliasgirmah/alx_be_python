9b84842ae152225a052ec703472589b165490b2c
Elias <eliasgirma.h@gmail.com>
2025-07-11 11:30:04 +0300

    simple interest script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#!/bin/main
+#variable declaration
+principal = 1000  # Principal amount
+rate = 0.05          # Rate of interest        
+time = 3          # Time in years
+# calculate simple interest
+simple_interest = (principal * rate * time) 
+
+# print the result
+print(f"The simple interest is: {simple_interest}")
