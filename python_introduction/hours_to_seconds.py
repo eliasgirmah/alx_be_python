a45e7d216bdd8c7ea0ee686968760db18f81d8dc
Elias <eliasgirma.h@gmail.com>
2025-07-11 13:30:16 +0300

    convet to hours to seconds cript

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script demonstrates basic arithmetic operations in Python.
 hours = 2
-seconds= hours * 3600
+seconds = hours * 3600
+
 # prirnt the result
 print(f"{hours} hour(s) is {seconds} seconds.")
