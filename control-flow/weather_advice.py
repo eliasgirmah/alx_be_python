3008342d02a6a7c22d84ad3485703f6a2a892685
Elias <eliasgirma.h@gmail.com>
2025-07-26 21:36:38 +0300

    weather recomendation system

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this script provides weather advice based on the current weather conditions
+weather= input(" What's the weather like today? (sunny/rainy/cold): ")
+
+if weather.lower()=="sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather.lower()=="rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather.lower()=="cold":
+    print(" Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
