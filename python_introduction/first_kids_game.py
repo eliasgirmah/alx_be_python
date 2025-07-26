9e9c4c155d5a08d87f616ebbc6716435f10d978a
Elias <eliasgirma.h@gmail.com>
2025-07-26 14:35:51 +0300

    first kids game

---
+++ python_introduction/first_kids_game.py
@@ -0,0 +1,9 @@
+name=input("please enter your name:")
+age= input("please enter your age:")
+grade=input("please enter your grade:")
+colors= input("please enter your colors example, red, blue, green")
+food= input("please enter your favorite food, example, pizza, burger, salad")
+game= input("please select your fovorite game, example, football, basketball, tennis")
+sotry=f"one day {name} was {age} years old and in {grade} grade student. {name} his favorite color is {colors}  and his favorite food is {food} and his favorite game is {game}."
+print("Here is your story:")
+print(sotry)
