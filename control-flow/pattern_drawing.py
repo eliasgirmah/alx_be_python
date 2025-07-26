db90014203c189934b63e27331c570b45f2837ef
Elias <eliasgirma.h@gmail.com>
2025-07-26 22:52:52 +0300

    update of pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,9 +1,12 @@
-#!/bin/bash
+
 #Utilize while loops and nested for loops to draw a simple text-based pattern.
-length= int(input("Enter the size of the pattern:"))
-while length >0:
-    for i in range (length):
-        print("*" * length, end=" ")
-        print()
-        i += 1
+length = int(input("Enter the size of the pattern: "))
+
+while length > 0:
+    for i in range(length):
+        for j in range(length):
+            print("*", end="")  
+        print()  # Moves to the next line
     break
+
+print("Pattern complete.")
