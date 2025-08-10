89a7ca3b0a3e382d43706a7c450af2d8ac09a5da
Elias <eliasgirma.h@gmail.com>
2025-08-10 18:44:25 +0300

    polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclass must implement this method")
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+import math
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
